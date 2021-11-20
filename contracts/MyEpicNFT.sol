5d7167430512ff3839a0278a1daca658efc963c5
daileytj <daileytj89@gmail.com>
2021-11-20 08:09:48 -0500

    Mint NFTs with static SVGs

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -14,7 +14,7 @@ contract MyEpicNFT is ERC721URIStorage {
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
 
-  string jsonLink = "https://jsonkeeper.com/b/QZJQ";
+  string base64SVG = "data:application/json;base64,ewogICJuYW1lIjogIkVwaWNMb3JkSGFtYnVyZ2VyIiwKICAiZGVzY3JpcHRpb24iOiAiQW4gTkZUIGZyb20gdGhlIGhpZ2hseSBhY2NsYWltZWQgc3F1YXJlIGNvbGxlY3Rpb24iLAogICJpbWFnZSI6ICJkYXRhOmltYWdlL3N2Zyt4bWw7YmFzZTY0LFBITjJaeUI0Yld4dWN6MGlhSFIwY0RvdkwzZDNkeTUzTXk1dmNtY3ZNakF3TUM5emRtY2lJSEJ5WlhObGNuWmxRWE53WldOMFVtRjBhVzg5SW5oTmFXNVpUV2x1SUcxbFpYUWlJSFpwWlhkQ2IzZzlJakFnTUNBek5UQWdNelV3SWo0S0lDQWdJRHh6ZEhsc1pUNHVZbUZ6WlNCN0lHWnBiR3c2SUhkb2FYUmxPeUJtYjI1MExXWmhiV2xzZVRvZ2MyVnlhV1k3SUdadmJuUXRjMmw2WlRvZ01UUndlRHNnZlR3dmMzUjViR1UrQ2lBZ0lDQThjbVZqZENCM2FXUjBhRDBpTVRBd0pTSWdhR1ZwWjJoMFBTSXhNREFsSWlCbWFXeHNQU0ppYkdGamF5SWdMejRLSUNBZ0lEeDBaWGgwSUhnOUlqVXdKU0lnZVQwaU5UQWxJaUJqYkdGemN6MGlZbUZ6WlNJZ1pHOXRhVzVoYm5RdFltRnpaV3hwYm1VOUltMXBaR1JzWlNJZ2RHVjRkQzFoYm1Ob2IzSTlJbTFwWkdSc1pTSStSWEJwWTB4dmNtUklZVzFpZFhKblpYSThMM1JsZUhRK0Nqd3ZjM1puUGc9PSIKfQ==";
 
   // We need to pass the name of our NFTs token and it's symbol.
   constructor() ERC721 ("SquareNFT", "SQUARE") {
@@ -30,7 +30,7 @@ contract MyEpicNFT is ERC721URIStorage {
     _safeMint(msg.sender, newItemId);
 
     // Set the NFTs data.
-    _setTokenURI(newItemId, jsonLink);
+    _setTokenURI(newItemId, base64SVG);
     console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
 
     // Increment the counter for when the next NFT is minted.
--- notes.md
+++ notes.md
@@ -1,3 +1,17 @@
-[JSON - Section_1 - Lesson_4](https://jsonkeeper.com/b/QZJQ)
-[First Contract Deployment - Etherscan](https://rinkeby.etherscan.io/address/0x9F5A77b9aE29Ecd50879786F36CcD7E51BcF3fd6)
-[First Mint On testnets Opensea](https://testnets.opensea.io/collection/squarenft-eswzi6ugv0)
+## Section_1
+
+[JSON - Lesson_4](https://jsonkeeper.com/b/QZJQ)
+
+[Contract Deployment - Etherscan](https://rinkeby.etherscan.io/address/0x9F5A77b9aE29Ecd50879786F36CcD7E51BcF3fd6)
+
+[Testnet Opensea Collection](https://testnets.opensea.io/collection/squarenft-eswzi6ugv0)
+
+---
+
+## Section_2
+
+[Contract Deployment - Lesson_2](https://rinkeby.etherscan.io/address/0x23c75c05e8c32b1e37c223ed6fcef2a5fc090e08)
+
+[Testnet Opensea Collection](https://testnets.opensea.io/collection/squarenft-8mw8176prp)
+
+---
